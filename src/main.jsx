48fe1a7319b2aee3e2a5b3123e18737b4f54584d
ansuman34 <160639331+ansuman34@users.noreply.github.com>
2026-01-31 01:22:46 +0530

    Configure for GitHub Pages deployment and fix routing

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/est-campus/">
       <App />
     </BrowserRouter>
   </StrictMode>,
